33ba4d8cac9f7acfa0261aaff66011bdfa427cb6
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-10 11:43:21 -0600

    added permitted attributes to egg_bundle serializer

--- app/serializers/egg_bundle_serializer.rb
+++ app/serializers/egg_bundle_serializer.rb
@@ -1,3 +1,3 @@
 class EggBundleSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :batch_number, :amount, :amount_fertilized, :amount_hatched, :start_date, :first_hatch, :init_temp, :init_humit, :lockdown_temp, :lockdown_humid, :flock_id
 end
