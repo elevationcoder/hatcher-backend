b1c06cbcde1ac126a3c90d1ffe3f8f66a18c0a82
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-09 18:21:12 -0600

    added update_quantity method to Flock model

--- app/models/flock.rb
+++ app/models/flock.rb
@@ -1,5 +1,20 @@
 class Flock < ApplicationRecord
     has_many :egg_bundles
     validates :name, presence: true
-    
+
+
+    def update_quantity(flock)
+        if flock.quantity == 'increase'
+            self.quantity == self.quantity + flock.quantity
+            self.save
+        elsif flock.quantity == 'decrease'
+            if self.quantity >= flock.quantity
+                self.save
+                self.quantity == self.quantity - flock.quantity
+            else
+                return "No chickens in this flock!"
+            end
+        end
+    end
+
 end
